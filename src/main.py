84a768724d519842b25e87071e383329f9fffd8d
Matthew Kehoe <mskehoe001@gmail.com>
2024-06-02 18:17:15 -0400

    Added hyper-param tuning, cross validation, and neural network analysis

--- src/main.py
+++ src/main.py
@@ -13,17 +13,17 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
 from sklearn.decomposition import PCA
 
 # Machine Learning
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold, GridSearchCV
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import BaggingClassifier, AdaBoostClassifier, RandomForestClassifier, VotingClassifier
+from sklearn.ensemble import BaggingClassifier, AdaBoostClassifier, RandomForestClassifier, VotingClassifier, GradientBoostingClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, make_scorer
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import roc_curve, auc
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
-from sklearn.svm import LinearSVC
+from sklearn.svm import LinearSVC, SVC
 import xgboost as xgb
 
 # Read the full contents of the dataframes
@@ -46,19 +46,16 @@ def convert_columns_to_numeric(df, columns):
         df[column] = pd.to_numeric(df[column], errors='coerce')
     return df
 
-def evaluate_classifiers(X_train, X_test, y_train, y_test):
+
+def evaluate_classifiers_cross_val(X, y):
     """
-    Evaluate multiple classifiers on the given training and test datasets.
+    Evaluate multiple classifiers on the given dataset using cross-validation.
 
     Parameters:
-    - X_train: pd.DataFrame
-        Training features.
-    - X_test: pd.DataFrame
-        Test features.
-    - y_train: pd.Series
-        Training labels.
-    - y_test: pd.Series
-        Test labels.
+    - X: pd.DataFrame
+        Features.
+    - y: pd.Series
+        Labels.
 
     Returns:
     - output: pd.DataFrame
@@ -66,8 +63,8 @@ def evaluate_classifiers(X_train, X_test, y_train, y_test):
     """
 
     # Preprocessing
-    numerical_features = X_train.select_dtypes(include=['int64', 'float64']).columns
-    categorical_features = X_train.select_dtypes(include=['object']).columns
+    numerical_features = X.select_dtypes(include=['int64', 'float64']).columns
+    categorical_features = X.select_dtypes(include=['object']).columns
 
     numerical_transformer = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy='median')),
@@ -85,16 +82,17 @@ def evaluate_classifiers(X_train, X_test, y_train, y_test):
     # Define classifiers
     classifiers = {
         'K-Nearest Neighbors': KNeighborsClassifier(),
-        'Logistic Regression': LogisticRegression(max_iter=1000),
+        'Logistic Regression': LogisticRegression(penalty='l2', max_iter=1000),
         'Support Vector Machine': LinearSVC(max_iter=10000),
-        'Decision Tree': DecisionTreeClassifier(),
-        'Bagging Decision Tree (Ensemble)': BaggingClassifier(DecisionTreeClassifier(), max_samples=0.5,
+        'Decision Tree': DecisionTreeClassifier(max_depth=5),  # Restricted depth to reduce overfitting
+        'Bagging Decision Tree (Ensemble)': BaggingClassifier(DecisionTreeClassifier(max_depth=5), max_samples=0.5,
                                                               max_features=1.0, n_estimators=10),
         'Boosting Decision Tree (Ensemble)': AdaBoostClassifier(
             DecisionTreeClassifier(min_samples_split=10, max_depth=4),
-            n_estimators=10, learning_rate=0.6, algorithm='SAMME'),
-        'Random Forest (Ensemble)': RandomForestClassifier(n_estimators=30, max_depth=8),
+            n_estimators=50, learning_rate=0.6, algorithm='SAMME'),
+        'Random Forest (Ensemble)': RandomForestClassifier(n_estimators=100, max_depth=5),
         'XGBoost': xgb.XGBClassifier(use_label_encoder=False, eval_metric='mlogloss'),
+        'Gradient Boosting': GradientBoostingClassifier(n_estimators=100, max_depth=3),
         'Voting Classifier (Ensemble)': VotingClassifier(estimators=[
             ('lr', LogisticRegression(max_iter=1000)),
             ('rf', RandomForestClassifier(n_estimators=30, max_depth=8)),
@@ -113,33 +111,38 @@ def evaluate_classifiers(X_train, X_test, y_train, y_test):
     results = []
     predictions = {}
 
+    skf = StratifiedKFold(n_splits=10)
     for label, clf in classifiers.items():
         start = datetime.datetime.now()
-        pipeline = Pipeline([
-            ('preprocessor', preprocessor),
-            ('classifier', clf)
-        ])
+        pipeline = make_pipeline(preprocessor, clf)
 
-        # Perform cross-validation
-        scores = cross_val_score(pipeline, X_train, y_train, scoring='accuracy', cv=5, n_jobs=1)
-        precision = cross_val_score(pipeline, X_train, y_train, scoring=scoring['precision'], cv=5, n_jobs=1)
-        recall = cross_val_score(pipeline, X_train, y_train, scoring=scoring['recall'], cv=5, n_jobs=1)
-        f1 = cross_val_score(pipeline, X_train, y_train, scoring=scoring['f1'], cv=5, n_jobs=1)
+        accuracy = []
+        precision = []
+        recall = []
+        f1 = []
 
-        pipeline.fit(X_train, y_train)
-        y_pred = pipeline.predict(X_test)
-        predictions[label] = y_pred
+        for train_idx, test_idx in skf.split(X, y):
+            X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
+            y_train, y_test = y.iloc[train_idx], y.iloc[test_idx]
+
+            pipeline.fit(X_train, y_train)
+            y_pred = pipeline.predict(X_test)
+            predictions[label] = y_pred
+
+            accuracy.append(accuracy_score(y_test, y_pred))
+            precision.append(precision_score(y_test, y_pred, average='weighted'))
+            recall.append(recall_score(y_test, y_pred, average='weighted'))
+            f1.append(f1_score(y_test, y_pred, average='weighted'))
 
         cv_time = str(datetime.datetime.now() - start)[:-3]
 
         result_dict = {
             'Model': label,
-            'Train Accuracy': pipeline.score(X_train, y_train),
-            'Test Accuracy': pipeline.score(X_test, y_test),
-            'Max CrossVal': scores.max(),
-            'Precision': precision.mean(),
-            'Recall': recall.mean(),
-            'F1 Score': f1.mean(),
+            'Train Accuracy': np.mean(accuracy),
+            'Test Accuracy': np.mean(accuracy),
+            'Precision': np.mean(precision),
+            'Recall': np.mean(recall),
+            'F1 Score': np.mean(f1),
             'Timespan': cv_time
         }
         results.append(result_dict)
@@ -148,6 +151,127 @@ def evaluate_classifiers(X_train, X_test, y_train, y_test):
     output = output.sort_values(by='Test Accuracy', ascending=False).reset_index(drop=True)
 
     return output, predictions
+
+
+def evaluate_classifiers_cross_val_and_tuning(X, y):
+    """
+    Evaluate multiple classifiers on the given dataset using cross-validation and hyperparameter tuning.
+
+    Parameters:
+    - X: pd.DataFrame
+        Features.
+    - y: pd.Series
+        Labels.
+
+    Returns:
+    - output: pd.DataFrame
+        DataFrame containing the performance metrics of each classifier.
+    """
+
+    numerical_features = X.select_dtypes(include=['int64', 'float64']).columns
+    categorical_features = X.select_dtypes(include=['object']).columns
+
+    numerical_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='median')),
+        ('scaler', StandardScaler())])
+
+    categorical_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', numerical_transformer, numerical_features),
+            ('cat', categorical_transformer, categorical_features)])
+
+    classifiers = {
+        'K-Nearest Neighbors': KNeighborsClassifier(),
+        'Logistic Regression': LogisticRegression(max_iter=1000),
+        'Support Vector Machine': LinearSVC(max_iter=10000),
+        'Decision Tree': DecisionTreeClassifier(),
+        'Bagging Decision Tree (Ensemble)': BaggingClassifier(DecisionTreeClassifier(), n_estimators=10),
+        'Boosting Decision Tree (Ensemble)': AdaBoostClassifier(DecisionTreeClassifier(), n_estimators=50, algorithm='SAMME'),
+        'Random Forest (Ensemble)': RandomForestClassifier(n_estimators=100),
+        'XGBoost': xgb.XGBClassifier(use_label_encoder=False, eval_metric='mlogloss'),
+        'Gradient Boosting': GradientBoostingClassifier(n_estimators=100),
+        'Voting Classifier (Ensemble)': VotingClassifier(estimators=[
+            ('lr', LogisticRegression(max_iter=1000)),
+            ('rf', RandomForestClassifier(n_estimators=30, max_depth=8)),
+            ('svm', LinearSVC(max_iter=10000)),
+            ('xgb', xgb.XGBClassifier(use_label_encoder=False, eval_metric='mlogloss'))
+        ], voting='hard')
+    }
+
+    param_grids = {
+        'K-Nearest Neighbors': {'classifier__n_neighbors': [3, 5, 7]},
+        'Logistic Regression': {'classifier__C': [0.1, 1.0, 10.0]},
+        'Support Vector Machine': {'classifier__C': [0.1, 1.0, 10.0]},
+        'Decision Tree': {'classifier__max_depth': [None, 10, 20], 'classifier__min_samples_split': [2, 10, 20]},
+        'Bagging Decision Tree (Ensemble)': {'classifier__n_estimators': [10, 50, 100]},
+        'Boosting Decision Tree (Ensemble)': {'classifier__n_estimators': [10, 50, 100],
+                                              'classifier__learning_rate': [0.01, 0.1, 1.0]},
+        'Random Forest (Ensemble)': {'classifier__n_estimators': [10, 50, 100],
+                                     'classifier__max_depth': [None, 10, 20]},
+        'XGBoost': {'classifier__n_estimators': [10, 50, 100], 'classifier__learning_rate': [0.01, 0.1, 1.0]},
+        'Gradient Boosting': {'classifier__n_estimators': [10, 50, 100], 'classifier__max_depth': [3, 5, 7]},
+        'Voting Classifier (Ensemble)': {}
+    }
+
+    scoring = {
+        'accuracy': 'accuracy',
+        'precision': make_scorer(precision_score, average='weighted'),
+        'recall': make_scorer(recall_score, average='weighted'),
+        'f1': make_scorer(f1_score, average='weighted'),
+    }
+
+    results = []
+    predictions = {}
+
+    skf = StratifiedKFold(n_splits=10)
+    for label, clf in classifiers.items():
+        start = datetime.datetime.now()
+        pipeline = Pipeline([('preprocessor', preprocessor), ('classifier', clf)])
+
+        accuracy = []
+        precision = []
+        recall = []
+        f1 = []
+
+        for train_idx, test_idx in skf.split(X, y):
+            X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
+            y_train, y_test = y.iloc[train_idx], y.iloc[test_idx]
+
+            param_grid = param_grids[label]
+            grid_search = GridSearchCV(pipeline, param_grid, scoring='accuracy', cv=5, n_jobs=1)
+            grid_search.fit(X_train, y_train)
+
+            best_estimator = grid_search.best_estimator_
+            y_pred = best_estimator.predict(X_test)
+            predictions[label] = y_pred
+
+            accuracy.append(accuracy_score(y_test, y_pred))
+            precision.append(precision_score(y_test, y_pred, average='weighted'))
+            recall.append(recall_score(y_test, y_pred, average='weighted'))
+            f1.append(f1_score(y_test, y_pred, average='weighted'))
+
+        cv_time = str(datetime.datetime.now() - start)[:-3]
+
+        result_dict = {
+            'Model': label,
+            'Train Accuracy': np.mean(accuracy),
+            'Test Accuracy': np.mean(accuracy),
+            'Precision': np.mean(precision),
+            'Recall': np.mean(recall),
+            'F1 Score': np.mean(f1),
+            'Timespan': cv_time
+        }
+        results.append(result_dict)
+
+    output = pd.DataFrame(results)
+    output = output.sort_values(by='Test Accuracy', ascending=False).reset_index(drop=True)
+
+    return output, predictions
+
 
 def pca_dimensionality_reduction(iris, X, y):
     """
@@ -182,11 +306,14 @@ def pca_dimensionality_reduction(iris, X, y):
     # Convert arrays to DataFrames
     X_train_pca_df = pd.DataFrame(X_train_pca)
     X_test_pca_df = pd.DataFrame(X_test_pca)
+    y_train_pca_df = pd.DataFrame(y_train_pca, columns=['species'])
+    y_test_pca = pd.DataFrame(y_test_pca, columns=['species'])
 
     # Evaluate classifiers on the PCA-transformed data
-    output_pca, predictions_pca = evaluate_classifiers(X_train_pca_df, X_test_pca_df, y_train_pca, y_test_pca)
+    output_pca, predictions_pca = evaluate_classifiers_cross_val(X_train_pca_df, y_train_pca)
 
     return output_pca, predictions_pca
+
 
 if __name__ == '__main__':
 
@@ -210,12 +337,11 @@ if __name__ == '__main__':
     y = iris_df['species']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
-    # Evaluate the model
-    output, predictions = evaluate_classifiers(X_train, X_test, y_train, y_test)
-    print('Results of ML algorithms without dimensionality reduction: \n')
+    # Evaluate the model for all features with cross validation
+    output, predictions = evaluate_classifiers_cross_val(X_train, y_train)
+    print('Results of ML algorithms with cross-validation: \n')
     print(output)
-    print('\nThe test accuracy for all of our models is 1 (100%). Therefore, we will need to reduce overfitting in ' \
-          'the test data.\n')
+    print('\n')
 
     # Evaluate the model with dimensionality reduction
     output_pca, predictions_pca = pca_dimensionality_reduction(iris_df, X, y)
@@ -229,8 +355,9 @@ if __name__ == '__main__':
     X_train_reduced, X_test_reduced, y_train_reduced, y_test_reduced = train_test_split(X_reduced, y, test_size=0.25,
                                                                                         random_state=42)
 
-    output_reduced, predictions_reduced = evaluate_classifiers(X_train_reduced, X_test_reduced, y_train_reduced,
-                                                               y_test_reduced)
+    output_reduced, predictions_reduced = evaluate_classifiers_cross_val(X_train_reduced, y_train_reduced)
     print('\n')
-    print('Results of ML algorithms for two most important features - pedal width and length: \n')
-    print(output_pca)
+    print('Results of ML algorithms for two most important features of pedal width and length: \n')
+    print(output_reduced)
+
+
